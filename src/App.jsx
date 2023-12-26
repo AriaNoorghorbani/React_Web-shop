ae0796349d62eb0bfc900bfc12d852b45e3ee15e
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-26 14:03:46 +0300

    use context more

--- src/App.jsx
+++ src/App.jsx
@@ -70,14 +70,12 @@ function App() {
   const ctxCart = {
     items: shoppingCart.items,
     addToCart: handleAddItemToCart,
+    updateCartItemQuantity: handleUpdateCartItemQuantity,
   };
 
   return (
     <CartContext.Provider value={ctxCart}>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
